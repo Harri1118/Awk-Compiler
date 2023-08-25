b37da71398fe9dd3b72fa5b2f6e159898481b456
Harri1118 <52708099+Harri1118@users.noreply.github.com>
2023-08-25 15:45:41 -0400

    Update UnitTests.java

--- UnitTests.java
+++ UnitTests.java
@@ -3,65 +3,66 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 
-public class LexerTest {
+public class UnitTest {
 
     private Lexer lexer;
 
-    /*
-     * @Before
-     * public void setUp() {
-     * lexer = new Lexer("", 0, 0);
-     * }
-     */
+    // Tests for empty cases
     @Test
-    public void test1() {
-        lexer = new Lexer("example", 0, 0);
+    public void test0() {
+        lexer = new Lexer("");
         lexer.Lex();
-        Token token = lexer.tokens.get(0);
-        assertEquals("[WORD(example)]", lexer.tokens);
-        assertEquals(0, token.getLine());
-        assertEquals(0, token.getPos());
+        assertEquals("[]", lexer.getTokens().toString());
     }
 
+    // Tests for a word
+    @Test
+    public void test1() {
+        lexer = new Lexer("example");
+        lexer.Lex();
+        Token token = lexer.getTokens().get(0);
+        assertEquals("[WORD(example)]", lexer.getTokens().toString());
+        assertEquals(0, token.getLine());
+        assertEquals(0, token.getStartPos());
+    }
+
+    // Tests for a number
     @Test
     public void test2() {
 
-        lexer = new Lexer("123", 0, 0);
+        lexer = new Lexer("123");
         lexer.Lex();
-        Token token = lexer.tokens.get(0);
-        assertEquals("[NUMBER(123)]", lexer.tokens);
+        Token token = lexer.getTokens().get(0);
+        assertEquals("[NUMBER(123)]", lexer.getTokens().toString());
         assertEquals(0, token.getLine());
-        assertEquals(0, token.getPos());
+        assertEquals(0, token.getStartPos());
     }
 
+    // Tests for a newline
     @Test
     public void test3() {
-        lexer = new Lexer("'\n'", 0, 0);
+        lexer = new Lexer("\n");
         lexer.Lex();
-        Token token = lexer.tokens.get(0);
-        assertEquals("[SEPARATOR]", lexer.tokens);
-        assertEquals(0, token.getLine());
-        assertEquals(0, token.getPos());
+        Token token = lexer.getTokens().get(0);
+        assertEquals("[SEPARATOR]", lexer.getTokens().toString());
+        assertEquals(1, token.getLine());
+        assertEquals(0, token.getStartPos());
     }
 
+    // Tests for a word, number, and newline
     @Test
     public void test4() {
+        lexer = new Lexer("test 5");
+        lexer.Lex();
+        assertEquals("[WORD(test), NUMBER(5)]", lexer.getTokens().toString());
     }
-    /*
-     * @Test
-     * public void test4() {
-     * 
-     * }
-     * 
-     * @Test
-     * public void test5() {
-     * 
-     * }
-     * 
-     * @Test
-     * public void test6() {
-     * 
-     * }
-     * lexer.tokens.forEach(token.toString() -> {System.out.println(element);});
-     */
+
+    //Tests for word and number, then number and word.
+    public void test5() {
+        lexer = new Lexer("5 test\ntest 5");
+        lexer.Lex();
+        assertEquals("[NUMBER(5), WORD(test)], SEPARATOR, WORD[test], NUMBER[5]", lexer.getTokens().toString());
+
+    }
+
 }
