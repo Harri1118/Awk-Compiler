4f74233b10a8adda57f0c85cb968d5de366f2d2e
Harri1118 <52708099+Harri1118@users.noreply.github.com>
2023-08-25 15:45:58 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,16 +3,18 @@ import java.nio.file.*;
 
 public class Main {
     public static void main(String[] args) {
-
+        /*
+         * Exists - Done, reads file with GetAllBytes - Done?, calls lex, prints tokens (10) - Done
+         */
         try {
             // Reads files with getAllBytes.
             Path myPath = Paths.get("test.awk");
             String content = new String(Files.readAllBytes(myPath));
             // Lexer object created to create a linkedlist and display the tokens
-            Lexer translator = new Lexer(content, 0, 0);
+            Lexer translator = new Lexer(content);
             // Prints Tokens
             translator.Lex();
-            System.out.println(translator.tokens);
+            System.out.println(translator.getTokens());
         } catch (IOException e) {
             e.printStackTrace();
         }
