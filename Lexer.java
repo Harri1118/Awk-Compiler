5bb8794bd45f27854a8513d19961f1968c090cbf
Harri1118 <52708099+Harri1118@users.noreply.github.com>
2023-08-29 17:53:20 -0400

    Update Lexer.java

--- Lexer.java
+++ Lexer.java
@@ -1,68 +1,71 @@
 import java.util.LinkedList;
-import java.lang.TypeNotPresentException;
 
 public class Lexer {
-    /*
-     * 
-     * Lex Exists, holds StringHandler, line number and position - Done
-     */
-
     // StringHandler initiated in this class. Used to parse through document and for
     // Lex to 'peek' through.
-    public StringHandler document;
-    // Line is inputted from lex into lexer to put into the Token object.
+    private StringHandler document;
+    // line variable used to count the amount of lines from document.
     private int line = 0;
-    // Pos is inputted from lex into lexer to put into the Token object.
+    // Position tracks the location of the pointer while parsing through
+    // StringHandler. Will be used for tokenizing.
     private int position = 0;
 
+    // LinkedList for the tokens to be stored.
     private LinkedList<Token> tokens = new LinkedList<>();
 
-    // Constrictor for Lexer. Takes a string, line, and position args in order to
-    // properly configure the type of token and values for the token.
+    // Constrictor for Lexer. Takes a string for document to utilize.
     public Lexer(String s) {
         document = new StringHandler(s);
     }
 
     public void Lex() {
-        try{
-        // Controller
-        boolean b = false;
+        // Try/catch expression for cases when document is invalid (empty document, full
+        // of invalid characters, etc)
+        try {
+            // finished boolean, will remain false as long as the status of the document is
+            // not done (document.isDone())
+            boolean finished = false;
 
-        // While loop which parses StringHandler (document)
-        while (b == false) {
-            if (document.IsDone() == true)
-                break;
-            char c = document.Peek(0);
+            // While loop which parses through the StringHandler (document)
+            while (finished == false) {
+                // Checks if the document is finished.
+                if (document.IsDone() == true)
+                    break;
+                // c variable initialized to analyze the sorting of the character.
+                char c = document.Peek(0);
 
-            // Checks if i is a space (ASCII decimal value 32)
-            if (c == 32 || c == 9 || c == 11 || c == 13) {
-                document.Swallow(1);
-                position++;
+                // Checks if c is a space (ASCII decimal value 32), or any other common invalid
+                // character
+                if (c == 32 || c == 9 || c == 11 || c == 13) {
+                    document.Swallow(1);
+                    position++;
+                }
+
+                // Checks if i is a newline (ASCII decimal value 10)
+                else if (c == 10) {
+                    document.Swallow(1);
+                    line++;
+                    tokens.add(processWord(String.valueOf(c)));
+                    position++;
+                }
+                // If document.peek(i) isn't a space or a newline, it gets checked whether it is
+                // a number or word.
+                else if (c > 32 && c < 127) {
+                    String w = document.PeekString(getNextCharLen());
+                    document.Swallow(w.length());
+                    if (notNum(w) == true) {
+                        tokens.add(processWord(w));
+                    } else
+                        tokens.add(processNumber(w));
+                    position = position + w.length();
+
+                }
+                // If no other case is plausable, it throws a typeNotPresent exception
+                else
+                    throw new Error("Invalid character at" + c);
             }
-
-            // Checks if i is a newline (ASCII decimal value 10)
-            else if (c == 10) {
-                document.Swallow(1);
-                line++;
-                tokens.add(processWord(String.valueOf(c)));
-                position++;
-            }
-            // If document.peek(i) isn't a space or a newline, it gets checked whether it is
-            // a number or word.
-            else if (c > 32 && c < 127) {
-                String w = document.PeekString(getNextCharLen());
-                document.Swallow(w.length());
-                if (notNum(w) == true) {
-                    tokens.add(processWord(w));
-                } else
-                    tokens.add(processNumber(w));
-                position = position + w.length();
-
-            } else
-                throw new TypeNotPresentException(null, null);
-        }}
-        catch(Exception e){
-            System.out.println("Empty file!");
+        } catch (Exception e) {
+            System.out.println("Invalid file!");
         }
 
     }
@@ -77,24 +80,26 @@ public class Lexer {
         }
     }
 
-    // method called to create a Token of a word type.
-    public Token processWord(String s) {
+    // Method called to create a Token of a word type.
+    private Token processWord(String s) {
         Token f = new Token(s, line, position);
         return f;
     }
 
-    // method called to create a Token of a number type.
-    public Token processNumber(String s) {
+    // Method called to create a Token of a number type.
+    private Token processNumber(String s) {
         double v = Double.valueOf(s);
         Token f = new Token(v, line, position);
         return f;
     }
 
-    public int getNextCharLen() {
+    // Method to calculate the length of a string in 'document'. If it reaches the
+    // end of a document, it will return i regardless.
+    private int getNextCharLen() {
         int i = 0;
         try {
-            // 'i' iterates by one each time a character is not decimal value 20 (based off
-            // raw ASCII characters)
+            // 'i' iterates by one each time a character is calid (ASCII value is greater
+            // than 32 and below 128)
             while (document.Peek(i) > 32 && document.Peek(i) < 127)
                 i = i + 1;
 
@@ -106,7 +111,8 @@ public class Lexer {
 
     }
 
-    public LinkedList<Token> getTokens(){
+    // Method used to return the tokens in the Lexer class.
+    public LinkedList<Token> getTokens() {
         return tokens;
     }
 }
