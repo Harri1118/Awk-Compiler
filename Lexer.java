d8509a94857c11f5af5f47d0ceafe6cf12059bda
Harri1118 <52708099+Harri1118@users.noreply.github.com>
2023-08-30 20:13:04 -0400

    Update Lexer.java

--- Lexer.java
+++ Lexer.java
@@ -4,8 +4,10 @@ public class Lexer {
     // StringHandler initiated in this class. Used to parse through document and for
     // Lex to 'peek' through.
     private StringHandler document;
+
     // line variable used to count the amount of lines from document.
     private int line = 0;
+
     // Position tracks the location of the pointer while parsing through
     // StringHandler. Will be used for tokenizing.
     private int position = 0;
@@ -18,101 +20,101 @@ public class Lexer {
         document = new StringHandler(s);
     }
 
+    // 'Lex()' Method to parse through the document and convert to tokens for the
+    // 'tokens' variable.
     public void Lex() {
-        // Try/catch expression for cases when document is invalid (empty document, full
-        // of invalid characters, etc)
+        // Try parsing through document until there is an error.
         try {
-            // finished boolean, will remain false as long as the status of the document is
-            // not done (document.isDone())
-            boolean finished = false;
-
-            // While loop which parses through the StringHandler (document)
-            while (finished == false) {
-                // Checks if the document is finished.
+            // Stopper boolean which doesn't stop the loop until it is finished.
+            boolean stopper = false;
+            // While loop to initiate parsing.
+            while (stopper == false) {
                 if (document.IsDone() == true)
                     break;
-                // c variable initialized to analyze the sorting of the character.
+                // char c initialized to hold the value of Peek at 0 (to see what character the
+                // document is on).
                 char c = document.Peek(0);
-
-                // Checks if c is a space (ASCII decimal value 32), or any other common invalid
-                // character
-                if (c == 32 || c == 9 || c == 11 || c == 13) {
-                    document.Swallow(1);
-                    position++;
+                // Checks if 'c' is a digit. Calls processNumber() and changes state.
+                if ((c == 46) || Character.isDigit(c)) {
+                    processNumber();
                 }
-
-                // Checks if i is a newline (ASCII decimal value 10)
+                // Checks if 'c' is alphabetic. Calls processWord() and changes state.
+                else if (Character.isAlphabetic(c)) {
+                    processWord();
+                }
+                // Checks if 'c' is a separator. Adds a separator token to tokens. Adds 1 to
+                // line, 1 to position, and the document swallows by 1.
                 else if (c == 10) {
+                    tokens.add(new Token(String.valueOf(c), position, line));
+                    position++;
                     document.Swallow(1);
-                    tokens.add(processWord(String.valueOf(c)));
                     line++;
+                }
+                // If 'c' isn't recognized by any of these, it will be ignored and the document
+                // swallows 1 and position adds by 1.
+                else {
+                    document.Swallow(1);
                     position++;
                 }
-                // If document.peek(i) isn't a space or a newline, it gets checked whether it is
-                // a number or word.
-                else if (c > 32 && c < 127) {
-                    String w = document.PeekString(getNextCharLen());
-                    document.Swallow(w.length());
-                    if (notNum(w) == true) {
-                        tokens.add(processWord(w));
-                    } else
-                        tokens.add(processNumber(w));
-                    position = position + w.length();
-
-                }
-                // If no other case is plausable, it throws a typeNotPresent exception
-                else
-                    throw new Error("Invalid character at" + c);
             }
-        } catch (Exception e) {
-            System.out.println("Invalid file!");
         }
-
-    }
-
-    // Checks if the incoming input from main is a number or not
-    private boolean notNum(String s) {
-        try {
-            Double.parseDouble(s);
-            return false;
-        } catch (Exception e) {
-            return true;
+        // If Lex() doesn't work (for whatever reason) it prints 'invalid file', and
+        // tells the line and position where the error occured.
+        catch (Exception e) {
+            System.out.println("Invalid file! Error at line " + line + ", and at position " + position);
         }
     }
 
-    // Method called to create a Token of a word type.
-    private Token processWord(String s) {
-        Token f = new Token(s, line, position);
-        return f;
-    }
-
-    // Method called to create a Token of a number type.
-    private Token processNumber(String s) {
-        double v = Double.valueOf(s);
-        Token f = new Token(v, line, position);
-        return f;
-    }
-
-    // Method to calculate the length of a string in 'document'. If it reaches the
-    // end of a document, it will return i regardless.
-    private int getNextCharLen() {
-        int i = 0;
-        try {
-            // 'i' iterates by one each time a character is calid (ASCII value is greater
-            // than 32 and below 128)
-            while (document.Peek(i) > 32 && document.Peek(i) < 127)
-                i = i + 1;
-
-            return i;
-
-        } catch (Exception e) {
-            return i;
+    // processWord() collects word characters until there is no more an alphabetic char.
+    public void processWord() {
+        // buffer used for adding all numbers into a string.
+        String buffer = "";
+        // while loop checks if the document is finished, as well as if the character is
+        // a digit or a period.
+        while (document.IsDone() == false && Character.isAlphabetic(document.Peek(0))) {
+            // 'c' initialized as the current char of the document, the index is also added
+            // by one.
+            char c = document.GetChar();
+            // 'c' added to buffer.
+            buffer += c;
         }
-
+        // number finally added to tokens.
+        tokens.add(new Token(buffer, position, line));
+        // position increments after Token is added.
+        position += buffer.length();
     }
 
-    // Method used to return the tokens in the Lexer class.
+    // processNumber() to check the number line by line until it can derive a valid
+    // double value.
+    public void processNumber() {
+        // foundPoint initialized to check if there's one decimal in the number.
+        boolean foundPoint = false;
+        // buffer used for adding all numbers into a string.
+        String buffer = "";
+        // while loop checks if the document is finished, as well as if the character is
+        // a digit or a period.
+        while (document.IsDone() == false && (Character.isDigit(document.Peek(0)) == true || document.Peek(0) == '.')) {
+            // 'c' initialized as the current char of the document, the index is also added
+            // by one.
+            char c = document.GetChar();
+            // 'c' added to buffer.
+            buffer += c;
+            // 'throws an error if c is a decimal and point is already found'.
+            if (c == '.' && foundPoint == true)
+                throw new Error("Not a valid number!");
+            // if c is '.' foundPoint is true.
+            else if (c == '.')
+                foundPoint = true;
+        }
+        // number finally added to tokens.
+        tokens.add(new Token(Double.parseDouble(buffer), position, line));
+        // position increments after Token is added.
+        position += buffer.length();
+    }
+
+    // Returns the 'tokens' variable.
     public LinkedList<Token> getTokens() {
         return tokens;
     }
+
 }
