32a2da96aacd44f79bb1110fd72b87312961bf24
Harri1118 <52708099+Harri1118@users.noreply.github.com>
2023-09-15 01:30:08 -0400

    Create TokenManager.java

---
+++ TokenManager.java
@@ -0,0 +1,29 @@
+package icsi311;
+
+import java.util.LinkedList;
+
+import icsi311.Token.TokenType;
+
+public class TokenManager {
+    private LinkedList<Token> tokens;
+
+    // tokens ahead and return the token if we aren’t past the end of the token list
+    private Token Peek(int j) {
+        return null;
+    }
+
+    // returns true if the token list is not empty
+    private boolean MoreTokens() {
+        return false;
+    }
+
+    // looks at the head of the list. If the token type of the head is the same as
+    // what was passed in,
+    // remove that token from the list and return it. In all other cases, returns
+    // Optional.Empty(). You will use
+    // this extensively.
+    private Token MathAndRemove(TokenType t) {
+        return null;
+    }
+
+}
