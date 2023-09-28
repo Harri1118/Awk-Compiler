808fec55007255d3f47ba580b2210a85e4d644b3
Harri1118 <52708099+Harri1118@users.noreply.github.com>
2023-09-27 21:30:41 -0400

    Create VariableReferenceNode.java

---
+++ VariableReferenceNode.java
@@ -0,0 +1,10 @@
+package icsi311;
+import java.util.Optional;
+public class VariableReferenceNode {
+    private String name;
+    private Optional<OperationNode> operation;
+
+    public String toString(){
+        return null;
+    }
+}
