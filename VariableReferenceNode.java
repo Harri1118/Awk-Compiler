110bfd42f7e0da19151e09808dac0960137d8245
Harri1118 <52708099+Harri1118@users.noreply.github.com>
2023-09-28 10:41:53 -0400

    Update VariableReferenceNode.java

--- VariableReferenceNode.java
+++ VariableReferenceNode.java
@@ -1,10 +1,15 @@
 package icsi311;
 import java.util.Optional;
-public class VariableReferenceNode {
+public class VariableReferenceNode extends Node{
     private String name;
-    private Optional<OperationNode> operation;
+    private Optional<Node> operation;
 
+    public VariableReferenceNode(String n){name = n;}
+    public VariableReferenceNode(String n, Optional<Node> o){
+        name = n;
+        operation = o;
+    }
     public String toString(){
-        return null;
+        return name + " : " + operation;
     }
 }
