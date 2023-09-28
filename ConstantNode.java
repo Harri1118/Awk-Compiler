cbfcea40298d2b5bb8cf7f079dd4fa47bbe111cf
Harri1118 <52708099+Harri1118@users.noreply.github.com>
2023-09-28 10:39:17 -0400

    Update ConstantNode.java

--- ConstantNode.java
+++ ConstantNode.java
@@ -2,7 +2,13 @@ package icsi311;
 
 public class ConstantNode extends Node {
     double value;
+    String strValue;
     public ConstantNode(double i){
         value = i;
+    }
+    public ConstantNode(String s){strValue = s;}
+
+    public String toString(){
+        return "CONSTANT("+strValue+")";
     }
 }
