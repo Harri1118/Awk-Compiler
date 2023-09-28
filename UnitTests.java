f361bc7123fffdfec8964424cebc1f790d2f9fd1
Harri1118 <52708099+Harri1118@users.noreply.github.com>
2023-09-27 21:30:25 -0400

    Update UnitTests.java

--- UnitTests.java
+++ UnitTests.java
@@ -1,19 +1,16 @@
 package icsi311;
 
-import static org.junit.Assert.assertEquals;
-
-import java.beans.Transient;
-
-import org.junit.Before;
-import org.junit.Test;
-
 import icsi311.Token.TokenType;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UnitTests {
 
     private Lexer lexer;
 
-    // Tests for empty cases
     @Test
     public void test0() {
         lexer = new Lexer("");
@@ -21,7 +18,6 @@ public class UnitTests {
         assertEquals("[]", lexer.getTokens().toString());
     }
 
-    // Tests for a word
     @Test
     public void test1() {
         lexer = new Lexer("example");
@@ -29,16 +25,13 @@ public class UnitTests {
         assertEquals("[WORD(example)]", lexer.getTokens().toString());
     }
 
-    // Tests for a number
     @Test
     public void test2() {
-
         lexer = new Lexer("123");
         lexer.Lex();
         assertEquals("[NUMBER(123)]", lexer.getTokens().toString());
     }
 
-    // Tests for a newline
     @Test
     public void test3() {
         lexer = new Lexer("\n");
@@ -46,7 +39,6 @@ public class UnitTests {
         assertEquals("[SEPARATOR]", lexer.getTokens().toString());
     }
 
-    // Tests for a word, number, and newline
     @Test
     public void test4() {
         lexer = new Lexer("test 5");
@@ -55,15 +47,12 @@ public class UnitTests {
     }
 
     @Test
-    // Tests for word and number, then number and word.
     public void test5() {
         lexer = new Lexer("5 test\ntest 5");
         lexer.Lex();
         assertEquals("[NUMBER(5), WORD(test), SEPARATOR, WORD(test), NUMBER(5)]", lexer.getTokens().toString());
-
     }
 
-    // Tests random awk command
     @Test
     public void test6() {
         lexer = new Lexer("$0 = tolower($0)");
@@ -80,7 +69,6 @@ public class UnitTests {
         assertEquals(Token.TokenType.CLOSEPAREN, lexer.getTokens().get(7).getType());
     }
 
-    // Tests if statements are all functional.
     @Test
     public void test7() {
         lexer = new Lexer(
@@ -108,7 +96,6 @@ public class UnitTests {
         assertEquals(Token.TokenType.FUNCTION, lexer.getTokens().get(18).getType());
     }
 
-    // Tests if operators are all functional.
     @Test
     public void test8() {
         lexer = new Lexer(">=  ++  --  <=  ==  !=  ^=  %=  *=  /=  +=  -=  !~   &&   >>   ||");
@@ -132,7 +119,6 @@ public class UnitTests {
         assertEquals(TokenType.OR, lexer.getTokens().get(15).getType());
     }
 
-    // Checks if operands are all functional.
     @Test
     public void test9() {
         lexer = new Lexer("{ } [ ] ( ) $ ~ = < > ! + ^ - ? : * / % ; \n | ,");
@@ -161,7 +147,6 @@ public class UnitTests {
         assertEquals(TokenType.SEPARATOR, lexer.getTokens().get(21).getType());
         assertEquals(TokenType.VERTBAR, lexer.getTokens().get(22).getType());
         assertEquals(TokenType.COMMA, lexer.getTokens().get(23).getType());
-
     }
 
     @Test
@@ -195,5 +180,206 @@ public class UnitTests {
         lexer.Lex();
         assertEquals("WORD(test)", lexer.getTokens().get(0).toString());
         assertEquals("SEPARATOR", lexer.getTokens().get(1).toString());
+    }
+
+    @Test
+    public void test14() {
+        lexer = new Lexer("_");
+        try {
+            lexer.Lex();
+        } catch (Exception e) {
+            assertEquals("Error at line 1\nNot a recognized character!", e.getMessage());
+        }
+    }
+
+    @Test
+    public void test15() {
+        lexer = new Lexer("\n2.22.");
+        try {
+            lexer.Lex();
+        } catch (Exception e) {
+            assertEquals("Error at line 2\nNot a valid number!", e.getMessage());
+        }
+    }
+
+    @Test
+    public void test16() {
+        lexer = new Lexer("&");
+        try {
+            lexer.Lex();
+        } catch (Exception e) {
+            assertEquals("Error at line 1\nCannot have single character '&'!", e.getMessage());
+        }
+    }
+
+    @Test
+    public void test17() {
+        lexer = new Lexer("\"\"\"");
+        try {
+            lexer.Lex();
+        } catch (Exception e) {
+            assertEquals("Error at line 1\nMust have an even number of quotes!", e.getMessage());
+        }
+    }
+
+    @Test
+    public void test18() {
+        lexer = new Lexer("\"\\\"test\"");
+        try {
+            lexer.Lex();
+        } catch (Exception e) {
+            assertEquals("Error at line 1\nMust have an even number of quotes in a string literal!", e.getMessage());
+        }
+    }
+
+    @Test
+    public void test19() {
+        lexer = new Lexer("$0");
+        lexer.Lex();
+        LinkedList<Token> testList = new LinkedList<Token>();
+        testList.add(new Token(Token.TokenType.DOLLAR, "$", 0, 1));
+        testList.add(new Token(Token.TokenType.NUMBER, "0", 0, 1));
+        TokenManager tokenManager = new TokenManager(lexer.getTokens());
+        assertEquals(Token.TokenType.DOLLAR, tokenManager.Peek(0).get().getType());
+        assertEquals(Token.TokenType.DOLLAR, tokenManager.MatchAndRemove(Token.TokenType.DOLLAR).get().getType());
+        testList.remove(0);
+        assertEquals(testList.get(0).toString(), tokenManager.Peek(0).get().toString());
+        assertEquals(true, tokenManager.MoreTokens());
+        tokenManager.MatchAndRemove(Token.TokenType.NUMBER);
+        assertEquals(false, tokenManager.MoreTokens());
+    }
+
+    @Test
+    public void test20() {
+        LinkedList<Token> statement = new LinkedList<Token>();
+        statement.add(new Token("SEPARATOR",0,1));
+        statement.add(new Token("Hello", 1, 2));
+        statement.add(new Token("SEPARATOR",0,2));
+        Parser p = new Parser(statement);
+    }
+
+    @Test
+    public void test21() {
+        try {
+            Lexer l = new Lexer("function test(a,b)\nBEGIN\nEND");
+            l.Lex();
+            Parser p = new Parser(l.getTokens());
+            ProgramNode pr = p.Parse();
+            assertEquals("BEGIN{[Condition: null, Statements: null]}\nOTHER{[]}\nEND{[Condition: null, Statements: null]}\nFunctions:\n[WORD(test)(WORD(a),WORD(b)) Statements: null]", pr.toString());
+        }
+        catch(Exception e){
+            System.out.println("Function is wrong.");
+        }
+    }
+
+    @Test
+    public void test22() throws Exception {
+        Lexer l = new Lexer("\nfunction test()\n\n\nBEGIN\nEND\n");
+        l.Lex();
+        Parser p = new Parser(l.getTokens());
+        ProgramNode pr = p.Parse();
+        assertEquals("BEGIN{[Condition: null, Statements: null]}\nOTHER{[]}\nEND{[Condition: null, Statements: null]}\nFunctions:\n[WORD(test)() Statements: null]", pr.toString());
+    }
+
+    @Test
+    public void test23() throws Exception {
+        Lexer l = new Lexer("BEGIN\nEND");
+        l.Lex();
+        Parser p = new Parser(l.getTokens());
+        ProgramNode pr = p.Parse();
+        assertEquals("BEGIN{[Condition: null, Statements: null]}\nOTHER{[]}\nEND{[Condition: null, Statements: null]}\nFunctions:\n[]", pr.toString());
+    }
+
+    @Test
+    public void test24(){
+        try{
+            Lexer l = new Lexer("function");
+            l.Lex();
+            Parser p = new Parser(l.getTokens());
+            p.Parse();
+        }
+        catch(Exception e){
+            assertEquals("Reached end of line before proper method return!",e.getMessage());
+        }
+    }
+
+    @Test
+    public void test25(){
+        try{
+            Lexer l = new Lexer("function()\n\n");
+            l.Lex();
+            Parser p = new Parser(l.getTokens());
+            p.Parse();
+        }
+        catch(Exception e){
+            assertEquals("Must have a valid method name!", e.getMessage());
+        }
+    }
+
+    @Test
+    public void test26(){
+        try{
+            Lexer l = new Lexer("function test\n\n");
+            l.Lex();
+            Parser p = new Parser(l.getTokens());
+            p.Parse();
+        }
+        catch(Exception e){
+            assertEquals("Function must have parenthesis!", e.getMessage());
+        }
+        try{
+            Lexer l = new Lexer("function test(\n\n");
+            l.Lex();
+            Parser p = new Parser(l.getTokens());
+            p.Parse();
+        }
+        catch(Exception e){
+            assertEquals("Function must have parenthesis!", e.getMessage());
+        }
+        try{
+            Lexer l = new Lexer("function test)\n\n");
+            l.Lex();
+            Parser p = new Parser(l.getTokens());
+            p.Parse();
+        }
+        catch(Exception e){
+            assertEquals("Function must have parenthesis!", e.getMessage());
+        }
+    }
+    @Test
+    public void test27(){
+        try{
+            Lexer l = new Lexer("BEGIN\n{\nEND");
+            l.Lex();
+            Parser p = new Parser(l.getTokens());
+            p.Parse();
+        }
+        catch(Exception e){
+            assertEquals("Action not recognized!", e.getMessage());
+        }
+    }
+    @Test
+    public void test28(){
+        try{
+            Lexer l = new Lexer("function t(t,)");
+            l.Lex();
+            Parser p = new Parser(l.getTokens());
+            p.Parse();
+        }
+        catch(Exception e){
+            assertEquals("Cannot have comma at the end of a parameter list!", e.getMessage());
+        }
+    }
+    @Test
+    public void test29(){
+        try{
+            Lexer l = new Lexer("function t(t");
+            l.Lex();
+            Parser p = new Parser(l.getTokens());
+            p.Parse();
+        }
+        catch(Exception e){
+            assertEquals("Must have parenthesis for proper parameter declaration!", e.getMessage());
+        }
     }
 }
