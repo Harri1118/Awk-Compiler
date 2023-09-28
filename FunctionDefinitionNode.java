8f3aa7d0bb92f21d74f8b4a5a27eb7b3422910d5
Harri1118 <52708099+Harri1118@users.noreply.github.com>
2023-09-27 21:27:24 -0400

    Update FunctionDefinitionNode.java

--- FunctionDefinitionNode.java
+++ FunctionDefinitionNode.java
@@ -1,5 +1,30 @@
 package icsi311;
 
-public class FunctionDefinitionNode {
+import java.util.LinkedList;
+public class FunctionDefinitionNode extends Node{
+private String name;
+private String[] parameters;
+private LinkedList<StatementNode> statements;
 
+public FunctionDefinitionNode(String n, String[] p, LinkedList<StatementNode> s){
+    name = n;
+    parameters = p;
+    statements = s;
+}
+
+    public String toString(){
+    if(parameters.length > 0)
+        return name + "(" + printParams() + ")" + " Statements: " + statements;
+    else
+        return name + "()" + " Statements: " + statements;
+    }
+
+    //Method to print params for the toString() method.
+public String printParams(){
+    String f = "";
+    for(int i = 0; i < parameters.length; i++)
+        f += parameters[i] + ",";
+    f = f.substring(0,f.length()-1);
+    return f;
+}
 }
