f2c339fe277f4279fa3c0e4f5fd60097c8be00f7
Harri1118 <52708099+Harri1118@users.noreply.github.com>
2023-09-27 21:28:17 -0400

    Create OperationNode.java

---
+++ OperationNode.java
@@ -0,0 +1,50 @@
+package icsi311;
+
+import java.util.Optional;
+
+public class OperationNode extends Node {
+    private Node Left;
+    private Optional<Node> Right;
+
+    public enum PossibleOperaions{
+        EQ,
+        NE,
+        LT,
+        LE,
+        GT,
+        GE,
+        AND,
+        OR,
+        NOT,
+        MATCH,
+        NOTMATCH,
+        DOLLAR,
+        PREINC,
+        POSTINC,
+        PREDEC,
+        POSTDEC,
+        UNARYPOS,
+        UNARYNEG,
+        IN,
+        EXPONENT,
+        ADD,
+        SUBTRACT,
+        MULTIPLY,
+        DIVIDE,
+        MODULO,
+        CONCATENATION
+
+    }
+
+    public OperationNode(Node l, Token.TokenType t, Optional<Node> r){
+        Left = l;
+        Right = r;
+    }
+
+    public OperationNode(Node l, Token.TokenType t){
+        Left = l;
+    }
+    public String toString(){
+        return null;
+    }
+}
