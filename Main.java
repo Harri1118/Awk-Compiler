8a9294bc65f6504c4aa286cfdf9c936077c1b678
Harri1118 <52708099+Harri1118@users.noreply.github.com>
2023-09-27 21:25:40 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,15 +10,20 @@ public class Main {
          */
         try {
             // Reads files with getAllBytes.
-            Path myPath = Paths.get("D:\\icsi311\\project2\\test.awk");
+            Path myPath = Paths.get("test.awk");
             String content = new String(Files.readAllBytes(myPath));
             // Lexer object created to create a linkedlist and display the tokens
             Lexer translator = new Lexer(content);
             // Prints Tokens
             translator.Lex();
-            System.out.println(translator.getTokens());
+            //Parser initiated and is printed when Parse() is called.
+            Parser parser = new Parser(translator.getTokens());
+            System.out.println(parser.Parse());
+
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 }
